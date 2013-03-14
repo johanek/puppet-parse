691daf6865fd73d3ed52f94181b0c93e5b19aa50
Johan van den Dorpe <johan.vandendorpe@gmail.com>
2013-03-14 09:47:09 +0000

    Add support for excluding paths from rake task

--- lib/puppet-parse/puppet-parse.rb
+++ lib/puppet-parse/puppet-parse.rb
@@ -6,6 +6,11 @@ require 'rake/tasklib'
 desc 'Run puppet-parse'
 task :parse do
   matched_files = FileList['**/*.pp']
+
+  if ignore_paths = PuppetParse.configuration.ignore_paths
+    matched_files = matched_files.exclude(*ignore_paths)
+  end
+
   run = PuppetParse::Runner.new
   puts run.run(matched_files.to_a).to_yaml
 end
