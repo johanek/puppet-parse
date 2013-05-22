bb5761f66f071b2688117ef5eaaeb792466b4c8e
pradels <les.pradels@gmail.com>
2013-05-22 18:19:41 +0200

    Parse complete info about nodes and classes.

--- lib/puppet-parse/parser.rb
+++ lib/puppet-parse/parser.rb
@@ -1,26 +1,212 @@
+require 'pp'
+
 class PuppetParse
   class Parser
 
     def initialize(file)
+      # AST representations of nodes and classes.
+      @nodes = []
+      @classes = []
+
+      # ASTs converted into ruby hash.
+      @nodes_hash = {}
+      @classes_hash = {}
+
       # Read file and return parsed object
-      pparser         = Puppet::Parser::Parser.new('production')
+      pparser = Puppet::Parser::Parser.new('test')
+
       if File.exists?(file)
         @file = File.expand_path(file)
-        pparser.import(@file)
 
-        # Find object in list of hostclasses
-        pparser.environment.known_resource_types.hostclasses.each do |x|
-          @object = x.last if x.last.file == @file
+        begin
+          pparser.import(@file)
+        rescue Puppet::ParseError => e
+          puts "Error while parsing: #{e.message}"
+          exit 1
         end
+
+        # Fill in nodes definitions.
+        @nodes = pparser.environment.known_resource_types.nodes
+
+        # Fill in classes defined in parsed file.
+        pparser.environment.known_resource_types.hostclasses.each do |klass|
+          next if klass.last.file != @file
+
+          @classes << klass.last
+        end
+
         # Find object in list of definitions
         pparser.environment.known_resource_types.definitions.each do |x|
           @object = x.last if x.last.file == @file
         end
-
       else
         'File does not exist'
       end
     end
+
+    # Just stripe away \" from string.
+    def strip_quotes(string)
+      string = string.chomp('\"').reverse.chomp('\"').reverse
+      string.chomp('"').reverse.chomp('"').reverse
+    end
+
+    def handle_vardef(hash, ast_node)
+      if ast_node.value == nil
+        rvalue = nil
+      else
+        rvalue = ast_node.value.to_s
+      end
+      rvalue = strip_quotes(rvalue)
+      hash[ast_node.name.to_s] = rvalue
+    end
+
+    def handle_resource(hash, ast_node)
+      ast_node.instances.each do |instance|
+        instance_title = strip_quotes(instance.title.to_s)
+        hash[instance_title] = {}
+
+        # Fill in each parameter.
+        instance.parameters.each do |parameter|
+          hash[instance_title][parameter.param.to_s] = strip_quotes(parameter.value.to_s)
+        end
+      end
+    end
+
+    def handle_resource_override(hash, ast_node)
+      hash[ast_node.object.to_s] = {}
+
+      ast_node.parameters.each do |parameter|
+        hash[ast_node.object.to_s][parameter.param.to_s] = strip_quotes(parameter.value.to_s)
+      end
+    end
+
+    def handle_function(hash, ast_node)
+      return if ast_node.name != "include"
+
+      argument_name = strip_quotes(ast_node.arguments[0].to_s)
+      hash[argument_name] = {}
+    end
+
+
+    ### Classes
+
+    def classes
+      @classes.each do |klass|
+
+        # Prepare new class hash.
+        @classes_hash[klass.name.to_s] = initialize_class_hash
+        @classes_hash[klass.name.to_s]["parent"] = klass.parent.to_s
+
+        next if klass.code == nil
+
+        # Walk through class definition.
+        klass.code.each do |ast|
+          #puts ast.class.to_s
+          case ast.class.to_s
+            when "Puppet::Parser::AST::VarDef"
+              handle_vardef(@classes_hash[klass.name.to_s]["variables"], ast)
+            when "Puppet::Parser::AST::Resource"
+              if @classes_hash[klass.name.to_s]["resources"][ast.type] == nil
+                @classes_hash[klass.name.to_s]["resources"][ast.type] = {}
+              end
+
+              handle_resource(@classes_hash[klass.name.to_s]["resources"][ast.type], ast)
+            when "Puppet::Parser::AST::ResourceOverride"
+              handle_resource_override(@classes_hash[klass.name.to_s]["resource_overrides"], ast)
+            else
+              next
+          end
+        end
+      end
+
+      @classes_hash
+    end
+
+    def initialize_class_hash
+      {
+        "variables"          => {},
+        "resources"          => {},
+        "resource_overrides" => {},
+
+        # Empty for now.
+        "resource_defaults"  => {},
+        "includes"           => {},
+      }
+    end
+
+
+
+    ### Nodes
+
+    # Return a hash describeing nodes parsed.
+    # 
+    # {
+    #   :name => {
+    #     :variables => {
+    #       :varname => value,
+    #       :varname => value,
+    #     }
+    #
+    #     :resources => {
+    #       :type => {
+    #         :name => {
+    #           :parameter => value,
+    #           :parameter => value,
+    #         },
+    #         :name => {
+    #           :parameter => value,
+    #           :parameter => value,
+    #         },
+    #       }
+    #     }
+    #
+    #     :includes => {
+    #       :name => {},
+    #       :name => {},
+    #     }
+    #   }
+    # }
+    def nodes
+      @nodes.each do |node|
+        node = node.pop
+
+        # Prepare new node hash.
+        @nodes_hash[node.name.to_s] = initialize_node_hash
+
+        # Walk through node definition.
+        node.code.each do |ast|
+          case ast.class.to_s
+            when "Puppet::Parser::AST::VarDef"
+              handle_vardef(@nodes_hash[node.name.to_s]["variables"], ast)
+            when "Puppet::Parser::AST::Resource"
+              if ast.type == "class"
+                resource_type = @nodes_hash[node.name.to_s]["includes"]
+              else
+                @nodes_hash[node.name.to_s]["resources"][ast.type] = {}
+                resource_type = @nodes_hash[node.name.to_s]["resources"][ast.type]
+              end
+
+              handle_resource(resource_type, ast)
+            when "Puppet::Parser::AST::Function"
+              handle_function(@nodes_hash[node.name.to_s]["includes"], ast)
+            else
+              # Just skip unknown AST types.
+              next
+          end
+        end
+      end
+
+      @nodes_hash
+    end
+
+    def initialize_node_hash
+      {
+        "variables" => {},
+        "resources" => {},
+        "includes"  => {},
+      }
+    end
+
 
     # Read parameters from parsed object, returns hash of parameters and default
     # values
--- lib/puppet-parse/runner.rb
+++ lib/puppet-parse/runner.rb
@@ -2,23 +2,34 @@ class PuppetParse
   class Runner
   
     def initialize
+      @output = {
+        "nodes"   => {},
+        "classes" => {},
+      }
     end
   
     def run(files)
-      output = {}
       files.each do |file|
-        content          = PuppetParse::Parser.new(file)
-        next if content.instance_variable_get('@object').nil? 
-        parameters = (defined? content.parameters) ? content.parameters.paramflat : nil
-        result           = {
-          content.klass  => {
-            'parameters' => parameters,
-            'docs'       => content.docs
-          }
-        }
-        output = output.merge(result)
+        content = PuppetParse::Parser.new(file)
+
+        nodes = content.nodes
+        @output["nodes"] = @output["nodes"].merge(nodes) if nodes != {}
+
+        classes = content.classes
+        @output["classes"] = @output["classes"].merge(classes) if classes != {}
+
+        #next if content.instance_variable_get('@object').nil? 
+        #parameters = (defined? content.parameters) ? content.parameters.paramflat : nil
+
+        #result = {
+          #content.klass  => {
+          #  'parameters' => parameters,
+          #  'docs'       => content.docs
+          #}
+        #}
+        #output = output.merge(classes)
       end
-      output
+      @output
     end
     
     
